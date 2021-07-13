32c4b9bf2d24ddce8d1bad8dd871d1872cf2617d
yoseli13 <yoselionel@gmail.com>
2021-07-13 19:30:17 -0400

    Nombre modulo biblioteca

--- modules/biblioteca/view.php
+++ modules/biblioteca/view.php
@@ -39,7 +39,7 @@ function validarExt()
 <section class="content-header">
   <!--<div id="visorArchivo"></div>-->
   <h2>
-  <i class="fa fa-folder-o icon-title"></i> Biblioteca
+  <i class="fa fa-folder-o icon-title"></i> Biblio
 
     <form action="database/excel_to_mysql_biblioteca.php" method="POST" enctype="multipart/form-data">
         <button class="btn btn-primary pull-right botones" title="Importar" name="archivoInput" data-toggle="tooltip">Importar</button>
