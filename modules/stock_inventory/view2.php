0cb0ba13306782ee9aeba7ce9c4ebff62a83bd0d
jcarri07 <carrizalesj5@gmail.com>
2021-11-26 15:56:36 -0400

    Validacion de Campos

--- modules/stock_inventory/view2.php
+++ modules/stock_inventory/view2.php
@@ -42,7 +42,7 @@
 
 <section class="content-header">
   <h1>
-    <i class="fa fa-edit icon-title"></i> Agregar equipos
+    <i class="fa fa-edit icon-title"></i> Reportes
   </h1>
   <ol class="breadcrumb">
     <li><a href="?module=start"><i class="fa fa-home"></i> Inicio </a></li>
